d1fa4e5d301affe3452948ba246d30dded63cbc9
arakviel <arakviel.smidt@pm.me>
2026-01-13 12:14:11 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,21 @@ export default defineNuxtConfig({
 
     nitro: {
         preset: 'github-pages',
+        prerender: {
+            routes: [
+                '/_ipx/_/images/se/domain-analysis.png',
+                '/_ipx/_/images/se/subdomains-types.png',
+                '/_ipx/_/images/se/subdomains-types-levels.png',
+                '/_ipx/_/images/se/knowledge-crunching.png',
+                '/_ipx/_/images/se/ubiquitous-language.png',
+                '/_ipx/_/images/se/map-model.png',
+                '/_ipx/_/images/se/leads-process.png',
+                '/_ipx/_/images/se/bounded-context.png',
+                '/_ipx/_/images/se/core-subdomain.png',
+                '/_ipx/_/images/se/generic-subdomain.png',
+                '/_ipx/_/images/se/supporting-subdomain.png',
+            ],
+        },
     },
 
     css: [
